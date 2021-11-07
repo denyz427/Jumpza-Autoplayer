754709a362234564700c8eb982863a510b95fc0b
denyz427 <84823943+denyz427@users.noreply.github.com>
2021-11-08 00:06:09 +0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,8 +10,8 @@ thetot = [[1,1],[1,1]]
 while True:
     #Screenshot1
     timeDiv = time.time()
-    sct_img1 = pyg.screenshot(region=(640, 344, 640, 735))   
-    sct_img = cv2.resize(np.array(sct_img1),(160,147))
+    sct_img1 = pyg.screenshot(region=(640, 279, 640, 800))   
+    sct_img = cv2.resize(np.array(sct_img1),(160,160))
     sct_img = cv2.cvtColor(sct_img, cv2.COLOR_RGB2HSV)
     masknorm = cv2.inRange(sct_img, (35,175,111), (43,255,167))
     masktram = cv2.inRange(sct_img, (86,142,146), (92,255,172))
@@ -24,7 +24,7 @@ while True:
     if len(cntstot) > 0:
         for cnt in cntstot:         
             if cv2.contourArea(cnt) > 16:
-                thetot.append((cv2.minEnclosingCircle(cnt)[0][0]*4+640,cv2.minEnclosingCircle(cnt)[0][1]*5+344))
+                thetot.append((cv2.minEnclosingCircle(cnt)[0][0]*4+640,cv2.minEnclosingCircle(cnt)[0][1]*5+279))
                 
     if (len(thetot)<2):
         continue
@@ -34,15 +34,15 @@ while True:
     if len(cntstotplayer)>0:
         for c in cntstotplayer:
             if cv2.contourArea(c)>50:
-                yp1 = int(cv2.minEnclosingCircle(c)[0][1]*5)+344
+                yp1 = int(cv2.minEnclosingCircle(c)[0][1]*5)+279
                 break
     
     #Screenshot2
     SecondSStime = time.time()
-    sct_img1 = pyg.screenshot(region=(640, 344, 640, 735)) 
+    sct_img1 = pyg.screenshot(region=(640, 279, 640, 800)) 
     timeDiv = SecondSStime-timeDiv
       
-    sct_img = cv2.resize(np.array(sct_img1),(160,147))
+    sct_img = cv2.resize(np.array(sct_img1),(160,160))
     sct_img = cv2.cvtColor(sct_img, cv2.COLOR_RGB2HSV)
     masknorm = cv2.inRange(sct_img, (35,175,111), (43,255,167))
     masktram = cv2.inRange(sct_img, (86,142,146), (92,255,172))
@@ -55,7 +55,7 @@ while True:
     if len(cntstot) > 0:
         for cnt in cntstot:      
             if cv2.contourArea(cnt) > 16:
-                thetot2.append((cv2.minEnclosingCircle(cnt)[0][0]*4+640,cv2.minEnclosingCircle(cnt)[0][1]*5+344))
+                thetot2.append((cv2.minEnclosingCircle(cnt)[0][0]*4+640,cv2.minEnclosingCircle(cnt)[0][1]*5+279))
                 
     if (len(thetot2)<2):
         win32api.SetCursorPos((int(thetot[1][0]),int(thetot[1][1])))
@@ -66,7 +66,7 @@ while True:
     if len(cntstotplayer)>0:
         for c in cntstotplayer:
             if cv2.contourArea(c)>50:
-                yp = int(cv2.minEnclosingCircle(c)[0][1]*5)+344
+                yp = int(cv2.minEnclosingCircle(c)[0][1]*5)+279
                 break
         
     if yp1<=yp and yp+84<thetot[1][1] and yp+257>thetot[1][1]:
